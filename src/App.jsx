f80e7617a972aac130a40c7e529e2a4cb68e95a0
microness <youjungho92@naver.com>
2025-07-11 14:02:46 +0900

    Phase0: #0-1. 리액트 프로젝트 생성, Tailwind 설치

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+// App이라는 이름의 함수형 컴포넌트
+// 해당 컴포넌트의 파일명은 App.jsx(js)로 만듦
+function App() {
+
+  return (
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
