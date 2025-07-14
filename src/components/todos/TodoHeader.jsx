bd7363d1af766d9c364a3809e598fdbe7a739f7a
microness <youjungho92@naver.com>
2025-07-14 10:37:01 +0900

    Phase1: #1-3. 할일 등록 기능을 위한 Modal 컴포넌트 구현

--- src/components/todos/TodoHeader.jsx
+++ src/components/todos/TodoHeader.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TodoFilter from './TodoFilter'
+import { createPortal } from 'react-dom';
+import Modal from '@/components/ui/Modal'
+import TodoForm from './TodoForm';
 
 const TodoHeader = () => {
+  // Modal의 열기/닫기 여부를 관리하는 상태값
+  const [openModal, open] = useState(false);
+  
   return (
     <div className="flex items-center justify-between mb-2" id="task-control">
-    <button className="px-6 py-2 font-semibold text-gray-100 bg-gray-800 border-none rounded cursor-pointer"
+    <button 
+            onClick={() => open(true)}
+            className="px-6 py-2 font-semibold text-gray-100 bg-gray-800 border-none rounded cursor-pointer"
             data-cy="add-todo-button">Add Todo
     </button>
+
+    {/* Modal이 생성되는 위치 */}
+    {openModal && createPortal(
+      // Modal컴포넌트에게 onClick이라는 이름의 props로 open 함수를 전달
+      <Modal onClose={() => open(false)}>
+        <TodoForm onClose={() => open(false)}/>
+      </Modal>, document.body
+    )}
 
     <TodoFilter />
   </div>
